9e76481dae526375d35f30d61094ee7b04627e4e
xdpiqbx <xdpiqbx@gmail.com>
2022-06-04 18:08:46 +0300

    added visualization banknotes

--- src/module_004/Task_017/ATM.java
+++ src/module_004/Task_017/ATM.java
@@ -14,7 +14,20 @@ public class ATM {
         return banknotesCount;
     }
 
+    public void countBanknotesVisualization(int sum){
+        int [] banknotes = new int []{500, 200, 100, 50, 20, 10, 5, 2, 1};
+        int len = banknotes.length;
+        int i = 0;
+        while (i < len) {
+            System.out.println("[" + banknotes[i] + " : " + sum/banknotes[i] + "] - rest : " + sum % banknotes[i]);
+            sum %= banknotes[i];
+            i++;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println(new ATM().countBanknotes(578));
+        int sum = 5478;
+        new ATM().countBanknotesVisualization(sum);
+        System.out.println(new ATM().countBanknotes(sum));
     }
 }
